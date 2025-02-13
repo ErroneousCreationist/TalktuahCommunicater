279a176dc55418e6c12b6cc00c10705b39ea50b0
ErroneousCreationiost <76930150+ErroneousCreationist@users.noreply.github.com>
2025-02-13 16:15:54 +1100

    Update Mainwindow Code-behind
    Added the framework for the buttons and input fields

--- TalktuahCommunicater1925/Views/MainWindow.axaml.cs
+++ TalktuahCommunicater1925/Views/MainWindow.axaml.cs
@@ -1,12 +1,53 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Input;
 
 namespace TalktuahCommunicater1925
 {
     public partial class MainWindow : Window
     {
+        public string Username, IP, Port;
         public MainWindow()
         {
+            Username = "";
+            IP = "";
+            Port = "";
             InitializeComponent();
+        }
+
+        public void EnterJoinPort(object sender, TextInputEventArgs e)
+        {
+            
+        }
+
+        public void EnterJoinIP(object sender, TextInputEventArgs e)
+        {
+            
+        }
+
+        public void EnterUsername(object sender, TextInputEventArgs e)
+        {
+            
+        }
+
+        public void JoinGeneralChat(object sender, RoutedEventArgs args)
+        {
+            
+        }
+
+        public void JoinPoliticalChat(object sender, RoutedEventArgs args)
+        {
+            
+        }
+
+        public void JoinRandomChat(object sender, RoutedEventArgs args)
+        {
+            
+        }
+
+        public void JoinCustomIP(object sender, RoutedEventArgs args)
+        {
+            
         }
     }
 }
